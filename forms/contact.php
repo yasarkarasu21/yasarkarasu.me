583c4aea9589e1c0183419fe022d1284a3e217a5
Yaşar KARASU <96866001+yasarkarasu21@users.noreply.github.com>
2022-01-10 21:13:04 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -2,7 +2,7 @@
 
   $receiving_email_address = 'yasarkarasu028@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/validate.js' )) {
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
@@ -15,16 +15,6 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
