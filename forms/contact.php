004eb20e31e4501064d09d2ab5170ad2f1d1f6aa
Yaşar KARASU <96866001+yasarkarasu21@users.noreply.github.com>
2022-01-10 21:59:01 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -10,12 +10,19 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
+
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-
+  
+  $contact->smtp = array(
+      'host' => 'smtp.gmail.com',
+      'username' => 'ykarasu028@gmail.com',
+      'password' => 'Yasar2099.',
+      'port' => '587'
+    );
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
